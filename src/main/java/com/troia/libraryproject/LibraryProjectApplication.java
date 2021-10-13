ac4fd18c2934fa06d46319e65c749db1181c3ac6
Tolga Aksoy <tolga.aksoy@bulutmd.com>
2021-10-13 17:52:12 +0300

    Start

---
+++ src/main/java/com/troia/libraryproject/LibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.troia.libraryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryProjectApplication.class, args);
+    }
+
+}
