cacad3d17612345e258b4a5d608b92290e51ed37
Tolga Aksoy <tolga.aksoy@bulutmd.com>
2021-10-23 19:12:49 +0300

    Added: languages to language enum

--- src/main/java/com/troia/libraryproject/model/Language.java
+++ src/main/java/com/troia/libraryproject/model/Language.java
@@ -1,4 +1,14 @@
 package com.troia.libraryproject.model;
 
 public enum Language {
+    TURKISH("TURKISH", "Türkçe"),
+    ENGLISH("ENGLISH", "İngilizce");
+
+    private final String value;
+    private final String turkishValue;
+
+    Language(String value, String turkishValue) {
+        this.value = value;
+        this.turkishValue = turkishValue;
+    }
 }
