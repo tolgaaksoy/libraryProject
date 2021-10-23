6eb052e095cd4c6a39f98237fb70b85be2f52b5d
Tolga Aksoy <tolga.aksoy@bulutmd.com>
2021-10-23 19:14:28 +0300

    Fixed: date-time serialization

--- src/main/java/com/troia/libraryproject/config/JacksonMapperConfig.java
+++ src/main/java/com/troia/libraryproject/config/JacksonMapperConfig.java
@@ -3,6 +3,8 @@ package com.troia.libraryproject.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +16,8 @@ public class JacksonMapperConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
         mapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return mapper;
     }
 }
